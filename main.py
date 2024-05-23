ba078ba7e5e85f556a5cb4b21d86d55ae656e6d1
francescogrillea <grilleafrancesco@gmail.com>
2024-05-23 11:47:23 +0200

    plot accuracy over time

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ from hoeffding_option_tree import HoeffdingOptionTreeClassifier
 from river.stream import iter_sklearn_dataset
 from sklearn import datasets
 from river.metrics import Accuracy
+import matplotlib.pyplot as plt
 
 model = HoeffdingOptionTreeClassifier(tau=1.5)
 dataset = datasets.load_iris()
 metric = Accuracy()
+plot_metric = []
 
-for x, y in iter_sklearn_dataset(dataset, shuffle=True, seed=42):
+for i, (x, y) in enumerate(iter_sklearn_dataset(dataset, shuffle=True, seed=42)):
     y_pred = model.predict_one(x)
     if y_pred is not None:
         metric.update(y, y_pred)
+        plot_metric.append(metric.get())
     model.learn_one(x, y)
+
 
 print(metric)
 print(metric.cm)
+# plt.plot(plot_metric)
+# plt.xlabel("Iteration")
+# plt.ylabel("Accuracy")
+# plt.title("Accuracy over Time")
+# plt.show()
